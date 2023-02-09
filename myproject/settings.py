40561388ac0a25503407548fe42c24d8708a3d5d
shopmall99 <serbible2021@gmail.com>
2023-02-09 20:08:22 +0900

    static root:pyton3,import os

--- myproject/settings.py
+++ myproject/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
